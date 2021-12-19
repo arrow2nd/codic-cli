7f4d10db86f902e65058273377f1baa54d6541fb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-19 17:34:58 +0900

    change: help text

--- cmd/config.go
+++ cmd/config.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 func (c *Cmd) newConfigCmd() *cobra.Command {
 	configCmd := &cobra.Command{
 		Use:   "config",
-		Short: "Operate the configuration",
+		Short: "Set various parameters",
 	}
 
 	configCmd.AddCommand(
